c49f5887c2e793bd9659f7a3e4f6a42b7758df3a
Fenriz <kastonbag552@gmail.com>
2026-08-24 02:34:26 +0300

    1

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -35,7 +35,7 @@ public abstract partial class SharedChatSystem : EntitySystem
 
     public const int VoiceRange = 10; // how far voice goes in world units
     public const int WhisperClearRange = 2; // how far whisper goes while still being understandable, in world units
-    public const int WhisperMuffledRange = 5; // how far whisper goes at all in world units
+    public const int WhisperMuffledRange = 5; // how far whisper goes at all, in world units
     public static readonly SoundSpecifier DefaultAnnouncementSound
         = new SoundPathSpecifier("/Audio/Announcements/announce.ogg");
 
