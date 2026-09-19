cfa6e1b419c33f8817df8ffda7af91b4bd65bb87
Fenriz <kastonbag552@gmail.com>
2026-09-19 05:41:43 +0300

    123

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -116,7 +116,7 @@ public sealed partial class CCVars
     ///     Prototype to use for map pool.
     /// </summary>
     public static readonly CVarDef<string>
-        GameMapPool = CVarDef.Create("game.map_pool", "DefaultMapPool", CVar.SERVERONLY);
+        GameMapPool = CVarDef.Create("game.map_pool", "RedStarMapPool", CVar.SERVERONLY); // RS14
 
     /// <summary>
     ///     The depth of the queue used to calculate which map is next in rotation.
