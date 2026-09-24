31ec8547ecc462780bdd82018caa823cdf6adf3c
Kofeechek <74077609+Kofeecheks@users.noreply.github.com>
2026-09-25 00:04:47 +0400

    Define swab protection for RedStar plants

---
+++ Content.Shared/_RedStar/Botany/RedStarPreventSwabbingComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._RedStar.Botany;
+
+/// <summary>
+/// Prevents pollen collection from plants that cannot be cross-pollinated.
+/// </summary>
+[RegisterComponent]
+public sealed partial class RedStarPreventSwabbingComponent : Component;
